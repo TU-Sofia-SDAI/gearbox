2e12614ea9c448e33c8abfc154a7e8262b46699c
todor-enikov <enigmaenikov@gmail.com>
2017-05-28 13:19:54 +0300

    Added testing project (Code.Tests) in the solution Code.sln and implemented two unit tests for class GearboxComputerLogic(for creating and throwing exception).

---
+++ Code.Tests/GearBoxComputerTests/Should.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using GearboxComputer;
+using GearboxContracts;
+using GearboxComputer.GearboxComputer;
+using Moq;
+
+namespace Code.Tests.GearBoxComputerTests
+{
+    [TestFixture]
+    public class Should
+    {
+        [Test]
+        public void CreateInstanceOfGearboxComputer()
+        {
+            // Arrange
+            var engineType = EngineType.Diesel;
+            var gearbox = new Mock<IGearBox>();
+            var listener = new Mock<IGearboxComputerListener>();
+            var communicator = new Mock<ICarComunication>();
+            gearbox.Setup(m => m.GearsCount).Returns(4);
+
+            // Act
+            var gearboxComputer = new GearboxComputerLogic(engineType, gearbox.Object, listener.Object, communicator.Object);
+
+            // Assert
+            Assert.IsInstanceOf<GearboxComputerLogic>(gearboxComputer);
+        }
+
+        [Test]
+        public void ThrowException_WithContainingSpecificMessage()
+        {
+            // Arrange
+            var engineType = EngineType.Diesel;
+            var gearbox = new Mock<IGearBox>();
+            var listener = new Mock<IGearboxComputerListener>();
+            var communicator = new Mock<ICarComunication>();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => new GearboxComputerLogic(engineType, gearbox.Object, listener.Object, communicator.Object));
+        }
+    }
+}
